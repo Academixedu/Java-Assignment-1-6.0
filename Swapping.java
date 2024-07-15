188bd085ce73a15657cb93a5f062983d37fe76b4
mprashanthkumar <mprashanthkumar18101996@gmail.com>
2024-07-15 13:37:42 +0530

    prashanth

--- Swapping.java
+++ Swapping.java
@@ -3,4 +3,13 @@ public class Swapping {
  int a=3;
  int b=45;
  int temp;
+ System.out.println("a = "+a);
+ System.out.println("b = "+b);
+
+ temp=a;
+ a=b;
+ b=temp;
+ System.out.println("after swap:");
+ System.out.println("a = "+a);
+ System.out.println("b = "+b);
     }}
