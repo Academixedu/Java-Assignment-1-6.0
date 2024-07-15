95534eaa232aaadd275b552205ae39cb314faee0
Ro-045 <pratapnaidu016@gmail.com>
2024-07-15 19:31:50 +0530

    pratap

--- Swapping.java
+++ Swapping.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Swapping {
     public static void main(String[] args) {
- int a=3;
- int b=45;
- int temp;
-    }}
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter value a:");
+        int a=sc.nextInt();
+        System.out.println("enter value b:");
+        int b=sc.nextInt();
+        // a=a+b;
+        // b=a-b;
+        // a=a-b;
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping values of a and b are:");
+        System.out.println("a:"+a);
+        System.out.println("b:"+b);
+        sc.close();
+    }
+    
+}
+
